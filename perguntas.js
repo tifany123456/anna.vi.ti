6230fc8fa8c6fd32026d02fb84a2e031f26bc581
tifany123456 <tifany.almeida@escola.pr.gov.br>
2024-10-04 19:52:51 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'matematica',
+    'Quanto é 9x8?',
+    'A resposta é 72'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Japão?',
+    'A capital do Japão é Tóquio'
+)
+
+criaCartao(
+    'biologia',
+    'Qual é a ação que a planta faz pra se alimentar?',
+    'fotossintese'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz Maçã em Inglês?',
+    'Maçã em ingles é Apple)'
+)
